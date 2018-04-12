d510ff2df27b18ce749bf892361d5e974823bdef
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-04-12 16:44:47 +0200

    Initial commit

---
+++ tests.js
@@ -0,0 +1,15 @@
+/**
+ * Rules applicable to test files.
+ *
+ * The rules in here are already included in the main config via an override
+ * config, but can also be used directly.
+ */
+module.exports = {
+  'extends': '@nighttrax',
+
+  'rules': {
+    'func-names': 0,
+    'no-unused-expressions': 0,
+    'no-console': 0
+  }
+};
