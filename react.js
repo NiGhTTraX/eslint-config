3ac9b5e2895ed44c990f7a765378b4fed0afdd93
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2018-12-27 15:42:16 +0100

    Disable some stylistic React rules

--- react.js
+++ react.js
@@ -4,6 +4,8 @@ module.exports = {
   ].map(require.resolve),
 
   'rules': {
+    'react/destructuring-assignment': 0,
+    'react/jsx-one-expression-per-line': 0,
     'react/jsx-filename-extension': 0,
     'react/jsx-wrap-multilines': 0,
     'react/prefer-stateless-function': 0,
