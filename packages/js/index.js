9801dabfe3ea8705ac18c6fe66014fd233b6a332
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2019-03-24 17:10:53 +0100

    Clean up config

--- packages/js/index.js
+++ packages/js/index.js
@@ -1,10 +1,7 @@
-/**
- * Rules for linting a JS lib.
- */
 module.exports = {
   'parser': 'babel-eslint',
 
   'extends': [
     '@nighttrax/eslint-config-base'
-  ].map(require.resolve),
+  ],
 };
