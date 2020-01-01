73dd6b8db46afdd483b83822da2651203450657a
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-01-01 16:46:01 +0100

    Replace npm commands in test script with yarn

--- test.sh
+++ test.sh
@@ -6,11 +6,12 @@ PROJECT=$1
 CONFIG=$2
 
 cd packages/$2
-npm link
+yarn link
 
 cd /tmp
 git clone https://github.com/NiGhTTraX/$1.git
 cd $1
-npm i --silent
-npm link @nighttrax/eslint-config-$2
-npm run lint
+
+yarn i --silent
+yarn link @nighttrax/eslint-config-$2
+yarn run lint
