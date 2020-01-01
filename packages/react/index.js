70799fd62e0bcc2801aeefdb7cbec04bbd2ef755
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2020-01-01 17:46:11 +0100

    Remove react style rules

--- packages/react/index.js
+++ packages/react/index.js
@@ -8,17 +8,9 @@ module.exports = {
 
   'rules': {
     'react/destructuring-assignment': 0,
-    'react/jsx-one-expression-per-line': 0,
     'react/jsx-filename-extension': 0,
-    'react/jsx-wrap-multilines': 0,
     'react/prefer-stateless-function': 0,
-    'react/jsx-first-prop-new-line': 0,
-    'react/jsx-max-props-per-line': [2, { when: 'multiline', maximum: 3 }],
     'react/jsx-no-bind': 0,
-    // TODO: https://github.com/yannickcr/eslint-plugin-react/issues/1176
-    'react/jsx-indent': 0,
-    // TODO: https://github.com/yannickcr/eslint-plugin-react/issues/1466
-    'react/jsx-closing-tag-location': 0,
     'react/sort-comp': [2, {
       order: [
         'displayName',
