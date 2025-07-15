d89d1f6a4ff27905d313987b9402c25ef70a55ed
Andrei Picus <NiGhTTraX@users.noreply.github.com>
2025-07-15 17:48:49 +0200

    refactor(react): Don't specify globals
    TypeScript should handle this.

--- packages/tsx/src/react.mjs
+++ packages/tsx/src/react.mjs
@@ -1,6 +1,3 @@
-import globals from "globals";
-import tsEslint from "typescript-eslint";
-import * as tsParser from "@typescript-eslint/parser";
 import react from "eslint-plugin-react";
 import jsxA11y from "eslint-plugin-jsx-a11y";
 import * as reactHooks from "eslint-plugin-react-hooks";
@@ -10,14 +7,7 @@ import { nighttraxTS } from "@nighttrax/eslint-config-ts";
  * @param {import('typescript-eslint').InfiniteDepthConfigWithExtends[]} configs
  */
 export const nighttraxReact = (...configs) =>
-  tsEslint.config([
-    nighttraxTS(),
-
-    {
-      files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
-      languageOptions: { globals: globals.browser, parser: tsParser },
-    },
-
+  nighttraxTS([
     react.configs.flat.recommended,
     react.configs.flat["jsx-runtime"],
     reactHooks.configs["recommended-latest"],
